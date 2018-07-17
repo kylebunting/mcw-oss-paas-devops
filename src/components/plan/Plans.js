8411d5fdf3716a6e389feb415ca307afe8b9d8f7
Ubuntu <kyle.bunting@gmail.com>
2018-07-17 19:22:26 +0000

    header change

--- src/components/plan/Plans.js
+++ src/components/plan/Plans.js
@@ -58,7 +58,7 @@ export default class Plans extends Component {
 
         return (
             <div class="container">
-                <h1>new test header</h1>
+                <h1>kyle test header</h1>
                 <Grid>
                     <div class="plans">
                         <Row>{planCatalog}</Row>
